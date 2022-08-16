e8008809978304412b09505d370e70180d3c6c61
Gianni Maratta <marattagian15@gmail.com>
2022-08-15 21:14:29 -0300

    changing read seeker to read write seeker and adding a test for it

--- file_system_store_test.go
+++ file_system_store_test.go
@@ -1,16 +1,18 @@
 package main
 
 import (
-	"strings"
+	"io"
+	"os"
 	"testing"
 )
 
 func TestFileSystemStore(t *testing.T) {
 	
 	t.Run("league frome a reader", func(t *testing.T) {
-		database := strings.NewReader(`[
+		database, cleanDatabase := createTempFile(t, `[
 		{"Name": "Cleo", "Wins": 10},
 		{"Name": "Chris", "Wins": 33}]`)
+		defer cleanDatabase()
 
 		store := FileSystemPlayerStore{database}
 
@@ -28,14 +30,30 @@ func TestFileSystemStore(t *testing.T) {
 	})
   
 	t.Run("get player score", func(t *testing.T) {
-		database := strings.NewReader(`[
+		database, cleanDatabase := createTempFile(t, `[
 		{"Name": "Cleo", "Wins": 10},
 		{"Name": "Chris", "Wins": 33}]`)
+		defer cleanDatabase()
 		
 		store := FileSystemPlayerStore{database}
 
 		got := store.GetPlayerScore("Chris")
 		want := 33
+		assertScoreEquals(t, got, want)
+	})
+
+	t.Run("store wins for existing players", func(t *testing.T) {
+		database, cleanDatabase := createTempFile(t, `[
+{"Name": "Cleo", "Wins": 10},
+{"Name": "Chris", "Wins": 33}]`)
+		defer cleanDatabase()
+
+		store := FileSystemPlayerStore{database}
+
+		store.RecordWin("Chris")
+
+		got := store.GetPlayerScore("Chris")
+		want := 34
 		assertScoreEquals(t, got, want)
 	})
 
@@ -46,4 +64,23 @@ func assertScoreEquals(t testing.TB, got, want int) {
 	if got != want {
 		t.Errorf("got score %d want %d", got, want)
 	}
+}
+
+func createTempFile(t testing.TB, initialData string) (io.ReadWriteSeeker, func()) {
+	t.Helper()
+	
+	tmpfile, err := os.CreateTemp("", "db")
+
+	if err != nil {
+		t.Fatalf("could not create temp file %v", err)
+	}
+
+	tmpfile.Write([]byte(initialData))
+
+	removeFile := func() {
+		tmpfile.Close()
+		os.Remove(tmpfile.Name())
+	}
+
+	return tmpfile, removeFile
 }
